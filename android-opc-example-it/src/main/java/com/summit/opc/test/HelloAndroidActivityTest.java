17971d65f01d83c22dbecfc7046b3f579d3383c4
Justin Smith <justin.smith@summitsystemsinc.com>
2013-07-09 08:15:27 -0500

    Initial commit, sample project from archetype

---
+++ android-opc-example-it/src/main/java/com/summit/opc/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.summit.opc.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.summit.opc.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
